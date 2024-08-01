2e773934d7e51a6263e39a092927fb1153a8ef3b
dishant987 <tankdishant2003@gmail.com>
2024-08-01 16:22:34 +0530

    change

--- client/src/components/forgotpassword/ForgotPasswordLink.jsx
+++ client/src/components/forgotpassword/ForgotPasswordLink.jsx
@@ -89,7 +89,7 @@ export default function ForgotPasswordLink() {
                 <Paper sx={{ padding: '25px', marginTop: '200px' }}>
                     <Link to={'/login'}>
                         <Button variant='outlined' startIcon={<ArrowBack />}>
-
+                            back
                         </Button>
                     </Link>
                     <Box
