4963b4878a9f34168579f9b4c5e8815c8e81693e
dishant987 <tankdishant2003@gmail.com>
2024-08-01 23:37:19 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://feedbackform-fronted-fetgoq751-dishant987s-projects.vercel.app",
+  origin:
+    "https://feedbackform-fronted.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
@@ -20,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -52,6 +53,6 @@ app.use((err, req, res, next) => {
 });
 
 // Set up a cron job
-cron.schedule('* * * * *', () => {
-  console.log('Cron job running every minute');
+cron.schedule("* * * * *", () => {
+  console.log("Cron job running every minute");
 });
