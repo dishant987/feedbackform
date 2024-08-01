dc946ced6472bc7f259cb3c781eeeb20c97e68f3
dishant987 <tankdishant2003@gmail.com>
2024-08-01 14:22:41 +0530

    change

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import { Link, useNavigate } from 'react-router-dom';
