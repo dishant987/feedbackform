3d439e093a21891ef72c87447a05e7fedbea7814
dishant987 <tankdishant2003@gmail.com>
2024-08-01 23:05:31 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ import cors from "cors";
 import connect from "./src/database/conn.js";
 import router from "./src/router/route.js";
 import cookieParser from "cookie-parser";
-import cron from "node-cron"; // Import the node-cron package
+import cron from "node-cron";
 
 config();
 
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://feedbackform-fronted-fetgoq751-dishant987s-projects.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
@@ -20,6 +20,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
